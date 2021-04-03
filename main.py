84418de4caa37b5127a8a9d642f031bdc57799e3
aantr <xgamestudio8@gmail.com>
2021-04-03 19:27:13 +0500

    #

--- main.py
+++ main.py
@@ -107,5 +107,5 @@ if __name__ == '__main__':
     thread.daemon = True
     thread.start()
 
-    port = os.environ.get('PORT', 80)
+    port = int(os.environ.get('PORT', 80))
     app.run(host='0.0.0.0', port=port)
