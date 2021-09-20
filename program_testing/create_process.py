97f2f01c4fe06e8ddcf0320419a452af2d7a84c4
aantr <xgamestudio8@gmail.com>
2021-09-20 22:45:29 +0500

    create_process fix

--- program_testing/create_process.py
+++ program_testing/create_process.py
@@ -61,8 +61,8 @@ def create_process(cmd: list, uid, private_folder, stdin, lang):
 
 def preexec(uid):
     def decorated():
-        return
-        os.setuid(uid)
+        if uid:
+            os.setuid(uid)
 
     return decorated
 
